460faa300c68237832c765c9bb3ea77ec847411f
alyssa.yang <alyssa.yang@temu.com>
2023-08-18 18:07:28 +0800

    feature: 修改yml

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({ data }: any) {
 // getServerSideProps 只在服务端调用，并不会运行在客户端。
 export async function getServerSideProps({ query }: { query: { id: string } }) {
   const res = await fetch(
-    "http://localhost:4000/api/web/content/get?id=" + (query.id || 2)
+    "http://clash-server.echoyore.tech/api/web/content/get?id=" + (query.id || 2)
   );
 
   const data = await res.json();
